9d5813e08f12ac0e71a2e8ccce8486f8ca2bd5b3
AdarshYadav-dev <adarsh98215@gmail.com>
2025-11-27 23:37:05 +0530

    learning-base-project

---
+++ project/script.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll('.category-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelectorAll('.category-btn').forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+    });
+});
